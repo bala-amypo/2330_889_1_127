bc7bbaa32a2c44d55c42fe2f706203b9f248cb8a
bala-amypo <bala@amypo.in>
2025-12-18 09:14:28 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Complaint.java
@@ -0,0 +1,72 @@
+package com.example.project.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Complaint {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String category;
+    private int priorityScore;
+    private LocalDateTime submittedOn;
+    private String user;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public int getPriorityScore() {
+        return priorityScore;
+    }
+    public void setPriorityScore(int priorityScore) {
+        this.priorityScore = priorityScore;
+    }
+    public LocalDateTime getSubmittedOn() {
+        return submittedOn;
+    }
+    public void setSubmittedOn(LocalDateTime submittedOn) {
+        this.submittedOn = submittedOn;
+    }
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
+    public Complaint(Long id, String title, String description, String category, int priorityScore,
+            LocalDateTime submittedOn, String user) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.priorityScore = priorityScore;
+        this.submittedOn = submittedOn;
+        this.user = user;
+    }
+    
+}
---
+++ src/main/java/com/example/demo/entity/PriorityRule.java
@@ -0,0 +1,46 @@
+package com.example.project.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class PriorityRule {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String category;
+    private Integer baseScore;
+    private String description;
+    public PriorityRule(Long id, String category, Integer baseScore, String description) {
+        this.id = id;
+        this.category = category;
+        this.baseScore = baseScore;
+        this.description = description;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public Integer getBaseScore() {
+        return baseScore;
+    }
+    public void setBaseScore(Integer baseScore) {
+        this.baseScore = baseScore;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
+}
