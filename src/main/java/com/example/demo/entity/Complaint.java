793a423a0a56dcbb4cfc331188bf0055ec1b224d
bala-amypo <bala@amypo.in>
2025-12-19 17:44:22 +0000

    User commit

--- src/main/java/com/example/demo/entity/Complaint.java
+++ src/main/java/com/example/demo/entity/Complaint.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+
 public class Complaint {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
