780a207d2a9bc4df829fe1fec47468e467f7c0fd
bala-amypo <bala@amypo.in>
2025-12-21 15:44:04 +0000

    User commit

--- src/main/java/com/example/demo/service/ComplaintService.java
+++ src/main/java/com/example/demo/service/ComplaintService.java
@@ -2,15 +2,14 @@ package com.example.demo.service;
 
 import java.util.List;
 import com.example.demo.entity.Complaint;
-import com.example.demo.entity.User;
 
 public interface ComplaintService {
 
-    Complaint createComplaint(Complaint complaint);
+    Complaint submitComplaint(Complaint complaint);
 
-    List<Complaint> getComplaintsForUser(User user);
+    List<Complaint> getUserComplaints(Long userId);
 
     List<Complaint> getPrioritizedComplaints();
 
-    Complaint updateComplaintStatus(Long complaintId, String status);
+    Complaint updateComplaintStatus(Long id, String status);
 }
