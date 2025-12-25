02f54eb4065609a54272cff99ac705c7d029e503
bala-amypo <bala@amypo.in>
2025-12-25 16:14:14 +0000

    User commit

--- src/main/java/com/example/demo/dto/ComplaintRequest.java
+++ src/main/java/com/example/demo/dto/ComplaintRequest.java
@@ -13,8 +13,21 @@ public class ComplaintRequest {
 
     public ComplaintRequest() {}
 
+    public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
+
+    public String getCategory() { return category; }
     public void setCategory(String category) { this.category = category; }
+
+    public String getChannel() { return channel; }
     public void setChannel(String channel) { this.channel = channel; }
+
+    public Complaint.Severity getSeverity() { return severity; }
+    public void setSeverity(Complaint.Severity severity) { this.severity = severity; }
+
+    public Complaint.Urgency getUrgency() { return urgency; }
+    public void setUrgency(Complaint.Urgency urgency) { this.urgency = urgency; }
 }
