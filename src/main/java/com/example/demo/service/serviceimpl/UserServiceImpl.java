15b155872911853e1d9d9c365ac330e9f86d7f82
bala-amypo <bala@amypo.in>
2025-12-26 08:14:40 +0000

    User commit

--- src/main/java/com/example/demo/service/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/serviceimpl/UserServiceImpl.java
@@ -1,5 +1,5 @@
 package com.example.demo.service.impl;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.security.JwtUtil;
@@ -16,6 +16,7 @@ public class UserServiceImpl implements UserService {
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
     
+    @Autowired
     public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
